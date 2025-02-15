dc9106d34591b5548a936fb9efd7ac5185d0e6c1
RyLoveQS <rylovequantstats@gmail.com>
2025-02-15 12:50:12 -0700

    Clean Up

--- liquidity_forecasting/liquidity_project/views.py
+++ liquidity_forecasting/liquidity_project/views.py
@@ -6,9 +6,9 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 
 # Import the run() functions from your services scripts.
-from liquidity_project_api.models.capital_pacing import run as run_capital_pacing
-from liquidity_project_api.models.liquidity_forecast import run as run_liquidity_forecast
-from liquidity_project_api.models.simulate_liquidity_monte import run as run_monte_carlo
+from liquidity_project.models.capital_pacing import run as run_capital_pacing
+from liquidity_project.models.liquidity_forecast import run as run_liquidity_forecast
+from liquidity_project.models.simulate_liquidity_monte import run as run_monte_carlo
 
 @require_http_methods(["GET"])
 def capital_pacing_api(request):
